9ae2f1fa4599085710ba6c6f8b925fe6211bf6c8
Pavle Vojinovic <vv.pavlee@gmail.com>
2019-03-19 21:23:32 +0100

    Are Equally Strong

---
+++ areEquallyStrong.js
@@ -0,0 +1,8 @@
+areEquallyStrong = (yourLeft, yourRight, friendsLeft, friendsRight) => {
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+    const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
+    }
